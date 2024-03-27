67bff6056cae08a467efd1bec5753f1e9c21f8b0
mariavo1 <daisykrn861@gmail.com>
2024-03-27 11:41:12 -0500

    generate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GymModule } from './gym/gym.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GymModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
